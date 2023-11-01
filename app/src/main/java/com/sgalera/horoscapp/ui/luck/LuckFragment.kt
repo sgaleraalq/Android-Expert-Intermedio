2414d3f10836e5b17cbeb9fcbf115c1622289427
sgaleraalq <sgaleraalq@gmail.com>
2023-11-02 00:13:21 +0100

    Luck fragment completed

--- app/src/main/java/com/sgalera/horoscapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/sgalera/horoscapp/ui/luck/LuckFragment.kt
@@ -1,6 +1,8 @@
 package com.sgalera.horoscapp.ui.luck
 
 import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,10 +14,13 @@ import android.view.animation.AnimationUtils
 import android.view.animation.DecelerateInterpolator
 import androidx.core.animation.doOnEnd
 import androidx.core.view.isVisible
+import com.aristidevs.horoscapp.ui.core.listeners.OnSwipeTouchListener
 import com.sgalera.horoscapp.R
 import com.sgalera.horoscapp.databinding.FragmentLuckBinding
+import com.sgalera.horoscapp.ui.providers.RandomCardProvider
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.Random
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class LuckFragment : Fragment() {
@@ -23,17 +28,52 @@ class LuckFragment : Fragment() {
     private var _binding: FragmentLuckBinding? = null
     private val binding get() = _binding!!
 
+    @Inject
+    lateinit var randomCardProvider: RandomCardProvider
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initUI()
     }
 
     private fun initUI() {
+        preparePrediction()
         initListeners()
     }
 
+    private fun preparePrediction(){
+        val currentLuck = randomCardProvider.getLucky()
+        currentLuck?.let {luck ->
+            val currentPrediction = getString(luck.text)
+            binding.tvLucky.text = currentPrediction
+            binding.ivLuckyCard.setImageResource(luck.image)
+            binding.tvShare.setOnClickListener { shareResult(currentPrediction) }
+        }
+    }
+
+    private fun shareResult(prediction: String) {
+        val sendIntent: Intent = Intent().apply{
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, prediction)
+            type = "text/plain"
+        }
+
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        startActivity(shareIntent)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
     private fun initListeners() {
         binding.ivRoulette.setOnClickListener{ spinRoulette() }
+
+        binding.ivRoulette.setOnTouchListener(object: OnSwipeTouchListener(requireContext()) {
+            override fun onSwipeRight(){
+                spinRoulette()
+            }
+            override fun onSwipeLeft(){
+                spinRoulette()
+            }
+        })
     }
 
     private fun spinRoulette() {
@@ -84,7 +124,11 @@ class LuckFragment : Fragment() {
 
     private fun showPremonitionView() {
         val disappearAnimation = AlphaAnimation(1.0f, 0.0f)
+        val appearAnimation = AlphaAnimation(0.0f, 1.0f)
+
         disappearAnimation.duration = 200
+        appearAnimation.duration = 1000
+
         disappearAnimation.setAnimationListener(object: Animation.AnimationListener{
             override fun onAnimationStart(animation: Animation?) {}
 
@@ -95,7 +139,9 @@ class LuckFragment : Fragment() {
 
             override fun onAnimationRepeat(animation: Animation?) {}
         })
+
         binding.clPreview.startAnimation(disappearAnimation)
+        binding.clPrediction.startAnimation(appearAnimation)
     }
 
     override fun onCreateView(
---
+++ app/src/main/java/com/sgalera/horoscapp/ui/model/LuckyModel.kt
@@ -0,0 +1,9 @@
+package com.sgalera.horoscapp.ui.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class LuckyModel (
+    @DrawableRes val image: Int,
+    @StringRes val text: Int
+)
