3a981dc23d19f14b7a2d1e9d6224c43f570157bd
sgaleraalq <sgaleraalq@gmail.com>
2023-11-02 22:55:08 +0100

    Finish test on UI

---
+++ app/src/androidTest/java/com/sgalera/horoscapp/ui/home/MainActivityTest.kt
@@ -0,0 +1,61 @@
+package com.sgalera.horoscapp.ui.home
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.sgalera.horoscapp.R
+import com.sgalera.horoscapp.ui.detail.HoroscopeDetailActivity
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
+class MainActivityTest {
+
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    var mainActivityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Before
+    fun setUp() {
+        hiltRule.inject()
+        Intents.init()
+    }
+
+    @After
+    fun tearDown(){
+        Intents.release()
+    }
+
+    @Test
+    fun when_mainactivity_is_created_then_open_luck_fragment() {
+        onView(withId(R.id.luckFragment)).perform(click())
+    }
+
+    @Test
+    fun when_horoscope_is_selected_then_open_detail() {
+        onView(withId(R.id.rvHoroscope)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click()
+            )
+        )
+
+        intended(hasComponent(HoroscopeDetailActivity::class.java.name))
+    }
+}
